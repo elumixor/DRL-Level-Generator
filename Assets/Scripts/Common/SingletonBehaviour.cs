1ab3a22ac345de788d4f63f263b285bee0c71c29
elumixor <vladogim97@gmaill.com>
2020-10-13 23:43:54 +0200

    Added a check for SingletonBehaviour that throws an error when another SingletonBehaviour is in the scene

--- Assets/Scripts/Common/SingletonBehaviour.cs
+++ Assets/Scripts/Common/SingletonBehaviour.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 namespace Common {
     public class SingletonBehaviour<T> : MonoBehaviour where T : MonoBehaviour {
         static T instance;
-        void Awake() => instance = GetComponent<T>();
+
+        void Awake() {
+            if (instance != null) throw new Exception($"There is more than one singleton behaviour of type [{typeof(T)}] in the scene.");
+
+            instance = GetComponent<T>();
+        }
     }
 }
