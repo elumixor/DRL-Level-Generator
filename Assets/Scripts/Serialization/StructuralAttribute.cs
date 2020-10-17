cb63aab2e405728c4bd5ed988ede2c1b58963f13
elumixor <vladogim97@gmaill.com>
2020-10-17 12:01:40 +0200

    Created StructuralAttribute that is used to get the number of floats in a type via reflection (used for state and action serialization)

---
+++ Assets/Scripts/Serialization/StructuralAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace Serialization {
+    /// <summary>
+    /// We are using this attribute to mark members of our state and action that will be used in training,
+    /// so that we know the size of data which we are sending and receiving from backend 
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Method)]
+    public class StructuralAttribute : Attribute {
+        // Filters correct member types
+        static Type GetMembersTypes(MemberInfo member) {
+            switch (member) {
+                case FieldInfo fieldInfo:
+                    return fieldInfo.FieldType;
+                case PropertyInfo propertyInfo:
+                    return propertyInfo.PropertyType;
+                case MethodInfo memberInfo:
+                    return memberInfo.ReturnType;
+                default:
+                    throw new Exception("Member is not a field, property or a method?");
+            }
+        }
+
+        // Maps members to sizes
+        static int GetFieldSize(Type t) { return t == typeof(Vector3) ? 3 : t == typeof(Vector2) ? 2 : t == typeof(float) ? 1 : 0; }
+
+        /// <summary>
+        /// Returns the total number of floats in a type. Searches for <see cref="Vector3"/>, <see cref="Vector2"/> and float members,
+        /// marked with the [Structural] attribute
+        /// </summary>
+        /// <param name="target">Type to be examined</param>
+        /// <returns>Total number of floats</returns>
+        public static int GetSize(Type target) {
+            var members = target.GetMembers(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+            var marked = members.Where(member => member.IsDefined(typeof(StructuralAttribute), true));
+            var memberTypes = marked.Select(GetMembersTypes);
+            var totalSize = memberTypes.Select(GetFieldSize).Sum();
+            return totalSize;
+        }
+    }
+}
---
+++ Assets/Testing/EditorTests/StructuralAttributeTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using NUnit.Framework;
+using UnityEditor.VersionControl;
+using UnityEngine;
+
+namespace Testing.StaticTests {
+    public class ReflectionTests {
+        [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Method)]
+        class MyAttribute : Attribute { }
+
+        abstract class Base {
+            [MyAttribute] float shouldNotBeInherited = 5;
+            [MyAttribute] protected float shouldBeInherited = 6;
+            [MyAttribute] protected abstract float inheritedMethod();
+        }
+
+        class C1 : Base {
+            float privateField = 5;
+            public float publicField = 5;
+            static float staticField = 5;
+            public float Method() => 5;
+            static float Getter => 5;
+
+            [My] float markedPrivateField = 1;
+            [My] public float markedPublicField = 2;
+            [My] static float markedStaticField = 3;
+            [My] public float markedMethod() => 4;
+            [My] static float markedGetter => 5;
+
+            protected override float inheritedMethod() { return 7; }
+        }
+
+        static IEnumerable<MemberInfo> GetMembers(Type t) =>
+            t.GetMembers(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic)
+                .Where(member => member.IsDefined(typeof(MyAttribute), true));
+
+        // A Test behaves as an ordinary method
+        [Test]
+        public void FindingMembersByCustomAttributePasses() {
+            // Use the Assert class to test conditions
+
+            var properties = GetMembers(typeof(C1)).ToArray();
+            Assert.AreEqual(7, properties.Length, "Should return exactly seven marked properties");
+        }
+
+        class C2 {
+            [My] float field = 1;
+            [My] string wrongField = "wrong";
+            [My] float Getter => 2;
+            [My] string WrongGetter => "wrong";
+            [My] float Method() => 3;
+            [My] string WrongMethod() => "wrong";
+            [My] Vector3 MethodVector3() => Vector3.one * 4;
+            [My] Vector2 MethodVector2() => Vector2.one * 5;
+        }
+
+        static bool CheckCorrectType(Type t) => t == typeof(float) || t == typeof(Vector3) || t == typeof(Vector2);
+
+        static IEnumerable<Type> GetMembersWithCorrectType(Type t) =>
+            GetMembers(typeof(C2)).Select(member => {
+                switch (member) {
+                    case FieldInfo fieldInfo:
+                        return fieldInfo.FieldType;
+                    case PropertyInfo propertyInfo:
+                        return propertyInfo.PropertyType;
+                    case MethodInfo memberInfo:
+                        return memberInfo.ReturnType;
+                    default:
+                        throw new Exception("Member is not a field, property or a method?");
+                }
+            }).Where(CheckCorrectType);
+
+        // A Test behaves as an ordinary method
+        [Test]
+        public void FindingMembersThatHaveFloatOrVec3ReturnTypePasses() {
+            // Use the Assert class to test conditions
+
+            var properties = GetMembersWithCorrectType(typeof(C2)).ToArray();
+            Assert.AreEqual(5, properties.Length, "Should return exactly 4 marked properties with the correct type");
+        }
+
+
+        [Test]
+        public void ShouldCalculateTheCorrectSizeOfAClass() {
+            var length = GetMembersWithCorrectType(typeof(C2)).Select(GetFieldSize).Sum();
+            Assert.AreEqual(length, 3 + 2 + 3, "Should return 8 floats");
+        }
+
+        static int GetFieldSize(Type t) { return t == typeof(Vector3) ? 3 : t == typeof(Vector2) ? 2 : 1; }
+    }
+}
