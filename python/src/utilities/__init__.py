10ef9ec7a4c7ad42bd6b55d8eefd2de19fa66606
elumixor <vladogim97@gmail.com>
2021-01-24 10:35:32 +0100

    Using vertex buffer, index buffers and uniform color to draw two triangles in example

--- python/src/utilities/__init__.py
+++ python/src/utilities/__init__.py
@@ -1,5 +1,11 @@
+import sys
+
 from .buffer import Buffer
 from .dot_dict import DotDict
 from .event import Event
 from .logging import *
 from .math_utilities import *
+
+
+def eprint(*args):
+    print(*args, file=sys.stderr)
