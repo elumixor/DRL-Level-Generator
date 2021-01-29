6fc7bded8fb9879877746c140f479476b65c8bf4
elumixor <vladogim97@gmail.com>
2021-01-29 10:16:30 +0100

    Added rotations

--- python/src/rendering/transform.py
+++ python/src/rendering/transform.py
@@ -3,19 +3,37 @@ from __future__ import annotations
 import numpy as np
 
 from rendering.point import Point
+from utilities import approx
 
 
 class Transform:
-    def __init__(self, local_position: Point, local_scale: Point):
+    def __init__(self, local_position: Point = Point.zero, local_scale: Point = Point.one, rotation: float = 0.0):
         self.local_position = local_position
         self.local_scale = local_scale
+        self.rotation = rotation
 
     @property
     def local_matrix(self):
         sx, sy = self.local_scale
         dx, dy = self.local_position
-        return np.array([
+
+        scale_translate = np.array([
             [sx, 0, dx],
             [0, sy, dy],
             [0, 0, 1],
         ], dtype=np.float32)
+
+        if approx(self.rotation % (2 * np.pi), 0.0):
+            return scale_translate
+
+        else:
+            cos = np.cos(self.rotation)
+            sin = np.sin(self.rotation)
+
+            rotate = np.array([
+                [cos, -sin, 0],
+                [sin, cos, 0],
+                [0, 0, 1],
+            ])
+
+            return scale_translate @ rotate
