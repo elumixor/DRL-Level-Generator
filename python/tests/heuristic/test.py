87d1440cf970238b45f9777c0aab416c43b68f70
elumixor <vladogim97@gmail.com>
2021-03-01 20:15:34 +0100

    Experiment with different x positions

--- python/tests/heuristic/test.py
+++ python/tests/heuristic/test.py
@@ -1,6 +1,4 @@
-import time
-
-import glfw
+import numpy as np
 
 import wandb
 from common import log
@@ -13,84 +11,68 @@ from utilities import approx
 if __name__ == '__main__':
     with RenderingContext(800, 600) as ctx:
         generator = DraggableGenerator(ctx)
+        trajectories = 1_000
 
         with PendulumEnvironment(ctx, generator, difficulty=0.5) as env:
             actor = HeuristicsPlayer()
-            randomness = actor.randomness
-            actor.randomness = None
 
-            while True:
-                state = env.reset()
-                obs = env.get_observation(state)
+            for x in np.linspace(-0.25, 0.25, 11):
+                run = wandb.init(project="Heuristic", name=f"Enemy X = {x:.2f}", config={
+                    "x": x
+                })
 
-                if randomness != actor.randomness:
+                log(f"x={x:.2f}", bold=True, r=100)
+                generator.enemy_x = x
+
+                for randomness in np.linspace(0, 1, 11):
                     log.good(f"Randomness set to {randomness:.2f}")
                     actor.randomness = randomness
 
                     R_best = None
                     R_worst = None
-                    run = None
                     difficulties = 0
                     num_trajectories = 0
 
-                    if run is not None:
-                        run.finish()
+                    for t in range(trajectories):
+                        state = env.reset()
+                        obs = env.get_observation(state)
 
-                    run = wandb.init(project="Heuristic", name=f"Randomness {randomness:.2f}", config={
-                        "randomness": randomness
-                    })
+                        max_len = 75
+                        i = 0
+                        done = False
+                        total_reward = 0
 
-                max_len = 75
-                i = 0
-                done = False
-                total_reward = 0
+                        while not done and i < max_len:
+                            generator.handle_input()
 
-                while not done and i < max_len:
-                    if ctx.is_key_pressed(glfw.KEY_ESCAPE):
-                        exit()
+                            action = actor.get_action(obs)
+                            obs, reward, done = env.transition(action)
+                            total_reward += reward
 
-                    if ctx.is_key_held(glfw.KEY_R):
-                        randomness = min(1.0, randomness + 0.01)
-                        print(f"Randomness={randomness:.2f}")
+                            i += 1
 
-                    elif ctx.is_key_held(glfw.KEY_D):
-                        randomness = max(0.0, randomness - 0.01)
-                        print(f"Randomness={randomness:.2f}")
+                        num_trajectories += 1
 
-                    env.render()
-                    generator.handle_input()
+                        if R_best is None or total_reward > R_best:
+                            R_best = total_reward
 
-                    action = actor.get_action(obs)
-                    obs, reward, done = env.transition(action)
-                    total_reward += reward
+                        if R_worst is None or total_reward < R_worst:
+                            R_worst = total_reward
 
-                    i += 1
+                        if not approx(R_worst, R_best):
+                            S = (total_reward - R_worst) / (R_best - R_worst)
+                            D = 1 - S
+                            difficulties += D
 
-                    time.sleep(0.02)
-
-                num_trajectories += 1
-
-                if R_best is None or total_reward > R_best:
-                    R_best = total_reward
-
-                if R_worst is None or total_reward < R_worst:
-                    R_worst = total_reward
-
-                wandb.log({
-                    "Total reward": total_reward,
-                    "R_best": R_best,
-                    "R_worst": R_worst,
-                })
-
-                if not approx(R_worst, R_best):
-                    S = (total_reward - R_worst) / (R_best - R_worst)
-                    D = 1 - S
-                    difficulties += D
-                    D_mean = difficulties / num_trajectories
-                    print(f"S={S:.2f} D={D:.2f} mean(D)={D_mean}")
+                        if t % (trajectories // 10) == 0:
+                            print(f"{t / trajectories * 100:.2}%")
 
                     wandb.log({
-                        "Difficulty (trajectory)": D,
-                        "Difficulty (run)": D_mean,
-                        "Trajectories count": num_trajectories
+                        "R_best": R_best,
+                        "R_worst": R_worst,
+                        "Difficulty": difficulties / num_trajectories,
+                        "Enemy x": x,
+                        "Randomness": randomness
                     })
+
+                run.finish()
