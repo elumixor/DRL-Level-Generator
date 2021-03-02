d72997b58c655e8dd821ed7e6c24e04f23fa6d68
elumixor <vladogim97@gmail.com>
2021-03-02 14:15:00 +0100

    Separate and shared R_best R_worst experiments

--- python/src/environments/pendulum/generators/draggable.py
+++ python/src/environments/pendulum/generators/draggable.py
@@ -9,7 +9,8 @@ from ..state import PendulumState
 
 class DraggableGenerator(PendulumGenerator):
     def __init__(self, ctx: RenderingContext, speed=0.01, fast_speed=0.1, min_x=-0.5, max_x=0.5,
-                 min_y=-0.25, max_y=0.75):
+                 min_y=-0.25, max_y=0.75, bob_radius=0.15, max_angle=np.deg2rad(30), angle=np.deg2rad(30),
+                 angular_speed=np.deg2rad(5), vertical_speed=0.02, connector_length=0.5):
         self.ctx = ctx
 
         self.speed = speed
@@ -24,26 +25,22 @@ class DraggableGenerator(PendulumGenerator):
         self.enemy_y = 0.25
         self.enemy_x = (max_x + min_x) / 2
 
+        self.bob_radius = bob_radius
+        self.max_angle = max_angle
+        self.connector_length = connector_length
+        self.vertical_speed = vertical_speed
+        self.angle = angle
+        self.angular_speed = angular_speed
+
         self.hint = Circle(Color(1, 0, 0, 0.3), position=Point(self.enemy_x, self.enemy_y),
                            scale=Point.one * 2 * self.enemy_radius, parent=ctx.main_scene)
 
     def generate(self, difficulty: float, seed: float) -> PendulumState:
-        bob_radius = 0.15
-        max_angle = np.deg2rad(30)
-        connector_length = 0.5
-        vertical_speed = 0.02
-
-        enemy_radius = self.enemy_radius
-        enemy_x = self.enemy_x
-        enemy_y = self.enemy_y
-
-        angle = np.deg2rad(30)
         position = 0
-        angular_speed = np.deg2rad(5)
 
-        return PendulumState(bob_radius, max_angle, connector_length, vertical_speed,
-                             enemy_x, enemy_y, enemy_radius,
-                             angle, position, angular_speed)
+        return PendulumState(self.bob_radius, self.max_angle, self.connector_length, self.vertical_speed,
+                             self.enemy_x, self.enemy_y, self.enemy_radius,
+                             self.angle, position, self.angular_speed)
 
     def handle_input(self):
         shift = self.ctx.is_key_held(glfw.KEY_LEFT_SHIFT) or self.ctx.is_key_held(glfw.KEY_RIGHT_SHIFT)
@@ -66,3 +63,17 @@ class DraggableGenerator(PendulumGenerator):
         self.enemy_y = clamp(self.enemy_y, self.min_y, self.max_y)
 
         self.hint.transform.local_position = Point(self.enemy_x, self.enemy_y)
+
+    @property
+    def config(self):
+        return {
+            "Enemy x": self.enemy_x,
+            "Enemy y": self.enemy_y,
+            "Enemy radius": self.enemy_radius,
+            "Bob radius": self.bob_radius,
+            "Max angle": self.max_angle,
+            "Connector length": self.connector_length,
+            "Vertical speed": self.vertical_speed,
+            "angle": self.angle,
+            "Angular speed": self.angular_speed,
+        }
---
+++ python/tests/heuristic/test_randomization_effect_shared.py
@@ -0,0 +1,94 @@
+import numpy as np
+
+import wandb
+from common import log
+from environments.pendulum import PendulumEnvironment
+from environments.pendulum.generators.draggable import DraggableGenerator
+from environments.pendulum.heuristics import HeuristicsPlayer
+from rendering import RenderingContext
+from utilities import approx
+
+if __name__ == '__main__':
+    with RenderingContext(800, 600) as ctx:
+        generator = DraggableGenerator(ctx)
+        trajectories = 1_000
+
+        with PendulumEnvironment(ctx, generator, difficulty=0.5) as env:
+            actor = HeuristicsPlayer()
+            enemy_x = -0.10
+            generator.enemy_x = enemy_x
+
+            R_best = None
+            R_worst = None
+
+            subdivisions = 11
+
+            total_rewards = [[0] * trajectories] * subdivisions
+            R_bests = [None] * subdivisions
+            R_worsts = [None] * subdivisions
+
+            randomnesses = np.linspace(0, 1, subdivisions)
+
+            run = wandb.init(project="Heuristic", name=f"Boundaries progression", config={
+                "Share": True,
+                **generator.config
+            })
+
+            for r, randomness in enumerate(randomnesses):
+                log.good(f"Randomness set to {randomness:.2f}")
+                actor.randomness = randomness
+
+                total_rewards[r] = [0] * trajectories
+
+                for t in range(trajectories):
+                    state = env.reset()
+                    obs = env.get_observation(state)
+
+                    max_len = 75
+                    i = 0
+                    done = False
+                    total_reward = 0
+
+                    while not done and i < max_len:
+                        action = actor.get_action(obs)
+                        obs, reward, done = env.transition(action)
+                        total_reward += reward
+
+                        i += 1
+
+                    if t % (trajectories // 10) == 0:
+                        print(f"{t / trajectories * 10:5.2}%")
+
+                    total_rewards[r][t] = total_reward
+
+                R_bests[r] = max([R_bests[r - 1] if r > 0 else float("-inf"), max(total_rewards[r])])
+                R_worsts[r] = min([R_worsts[r - 1] if r > 0 else float("inf"), min(total_rewards[r])])
+
+                wandb.log({"R best": R_bests[r], "R worst": R_worsts[r]})
+
+            run.finish()
+
+            for r in range(subdivisions):
+                randomness = randomnesses[r]
+
+                run = wandb.init(project="Heuristic", name=f"Randomness ({randomness:.2f}), shared", config={
+                    "Share": True,
+                    **generator.config
+                })
+
+                for _ in range(0, r):
+                    wandb.log({"Difficulty": None})
+
+                for i in range(r, subdivisions):
+                    R_best, R_worst = R_bests[i], R_worsts[i]
+
+                    difficulty_run = None
+                    if not approx(R_worst, R_best):
+                        trajectories_difficulties = [(1 - (total_reward - R_worst) / (R_best - R_worst))
+                                                     for total_reward in total_rewards[r]]
+
+                        difficulty_run = sum(trajectories_difficulties) / trajectories
+
+                    wandb.log({"Difficulty": difficulty_run})
+
+                run.finish()
