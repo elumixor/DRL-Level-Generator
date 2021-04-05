608eaf3785ca1311319ee52b7eed6fe58a761bf3
elumixor <vladogim97@gmail.com>
2021-04-05 22:41:44 +0200

    LReLU fixes learning issues

--- python/src/generators/probabilistic.py
+++ python/src/generators/probabilistic.py
@@ -1,6 +1,6 @@
 from typing import Literal
 
-from torch.nn import Module, Linear, Softplus
+from torch.nn import Module, Linear, Softplus, LeakyReLU
 
 
 class ProbabilisticGenerator(Module):
@@ -11,6 +11,9 @@ class ProbabilisticGenerator(Module):
     def __init__(self, min_std: float, std_constrain: Literal["clamp", "softplus"] = "softplus"):
         super().__init__()
         self.l1 = Linear(1, 2)
+
+        self.lrelu = LeakyReLU()
+
         self.mean = Linear(2, 1)
         self.std = Linear(2, 1)
 
@@ -32,7 +35,8 @@ class ProbabilisticGenerator(Module):
             raise ValueError(f"Std constrain should be either \"clamp\" or \"softplus\". Received \"{std_constrain}\"")
 
     def forward(self, x):
-        x = self.l1(x).relu()
+        x = self.l1(x)
+        x = self.lrelu(x)
 
         mean = self.mean(x)
 
